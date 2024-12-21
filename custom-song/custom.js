4d892407868a4fbae06425e2525429d84e365046
irfanaslam26 <irfan@thaagam.email>
2024-12-21 15:01:51 +0530

    index page 1st update

--- custom-song/custom.js
+++ custom-song/custom.js
@@ -158,3 +158,21 @@ document.addEventListener('DOMContentLoaded', function () {
         closeCurrentAudio();
     });
 });
+
+
+
+const swiper = document.querySelector('.swpr');
+swiper.breakpoints = {
+  320: {
+    slidesPerView: 1,
+    spaceBetween: 10,
+  },
+  640: {
+    slidesPerView: 2,
+    spaceBetween: 20,
+  },
+  1024: {
+    slidesPerView: 3,
+    spaceBetween: 30,
+  },
+};
